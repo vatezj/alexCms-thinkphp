bed0dae33240afea237672c9378871711ae373d4
zoujungao <1823297006@qq.com>
2022-04-20 18:43:44 +0800

    f

---
+++ app/controller/admin/UserController.php
@@ -0,0 +1,93 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\controller\admin;
+
+
+use Curl\Curl;
+use think\Request;
+
+class UserController
+{
+    /**
+     * 显示资源列表
+     *
+     * @return \think\Response
+     */
+    public function index()
+    {
+
+
+        $curl = new Curl();
+        $curl->get('https://192.168.0.113:3100/basic-api/system/getMenuList?_t=1650443351778');
+        $curl->close();
+        dd($curl);
+
+    }
+
+    /**
+     * 显示创建资源表单页.
+     *
+     * @return \think\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * 保存新建的资源
+     *
+     * @param  \think\Request  $request
+     * @return \think\Response
+     */
+    public function save(Request $request)
+    {
+        //
+    }
+
+    /**
+     * 显示指定的资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function read($id)
+    {
+        //
+    }
+
+    /**
+     * 显示编辑资源表单页.
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * 保存更新的资源
+     *
+     * @param  \think\Request  $request
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * 删除指定资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+}
